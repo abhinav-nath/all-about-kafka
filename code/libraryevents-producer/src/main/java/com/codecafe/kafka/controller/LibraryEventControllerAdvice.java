d340486e8bb2a4ccca9f3fe2579dc26ab84df4af
Abhinav Nath <48696735+abhinav-nath@users.noreply.github.com>
2021-05-30 23:05:12 +0530

    added ControllerAdvice and unit tests for bad request errors

---
+++ code/libraryevents-producer/src/main/java/com/codecafe/kafka/controller/LibraryEventControllerAdvice.java
@@ -0,0 +1,34 @@
+package com.codecafe.kafka.controller;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import lombok.extern.slf4j.Slf4j;
+
+@ControllerAdvice
+@Slf4j
+public class LibraryEventControllerAdvice {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleRequestBody(MethodArgumentNotValidException ex) {
+
+        List<FieldError> errorList = ex.getBindingResult().getFieldErrors();
+
+        String errorMessage = errorList.stream()
+                                       .map(fieldError -> fieldError.getField() + " - " + fieldError.getDefaultMessage())
+                                       .sorted()
+                                       .collect(Collectors.joining(", "));
+
+        log.info("errorMessage : {}", errorMessage);
+
+        return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
+    }
+
+}
--- code/libraryevents-producer/src/test/java/com/codecafe/kafka/unit/controller/LibraryEventsControllerUnitTest.java
+++ code/libraryevents-producer/src/test/java/com/codecafe/kafka/unit/controller/LibraryEventsControllerUnitTest.java
@@ -2,6 +2,7 @@ package com.codecafe.kafka.unit.controller;
 
 import static org.mockito.ArgumentMatchers.isA;
 import static org.mockito.Mockito.doNothing;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -70,10 +71,9 @@ public class LibraryEventsControllerUnitTest {
 
         doNothing().when(libraryEventProducer).sendLibraryEventAsync(isA(LibraryEvent.class));
 
-        mockMvc.perform(post("/v1/libraryevent")
-                .content(json)
-                .contentType(MediaType.APPLICATION_JSON))
-        .andExpect(status().isBadRequest());
+        String expectedErrorMessage = "book - must not be null";
+
+        testBadRequestError(json, expectedErrorMessage);
     }
 
     @Test
@@ -93,6 +93,86 @@ public class LibraryEventsControllerUnitTest {
                 .content(json)
                 .contentType(MediaType.APPLICATION_JSON))
         .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("bookIdIsMandatory")
+    void createLibraryEvent_BookIdMandatory() throws Exception {
+
+        Book book = Book.builder()
+                        .bookId(null)
+                        .bookAuthor("John Wick")
+                        .bookName("Guns and Cars")
+                        .build();
+
+        LibraryEvent libraryEvent = LibraryEvent.builder()
+                                                .libraryEventId(null)
+                                                .book(book)
+                                                .build();
+
+        String json = objectMapper.writeValueAsString(libraryEvent);
+
+        doNothing().when(libraryEventProducer).sendLibraryEventAsync(isA(LibraryEvent.class));
+        
+        String expectedErrorMessage = "book.bookId - must not be null";
+
+        testBadRequestError(json, expectedErrorMessage);
+    }
+
+    @Test
+    @DisplayName("bookNameIsMandatory")
+    void createLibraryEvent_BookNameMandatory() throws Exception {
+
+        Book book = Book.builder()
+                        .bookId(101)
+                        .bookAuthor("John Wick")
+                        .bookName("")
+                        .build();
+
+        LibraryEvent libraryEvent = LibraryEvent.builder()
+                                                .libraryEventId(null)
+                                                .book(book)
+                                                .build();
+
+        String json = objectMapper.writeValueAsString(libraryEvent);
+
+        doNothing().when(libraryEventProducer).sendLibraryEventAsync(isA(LibraryEvent.class));
+
+        String expectedErrorMessage = "book.bookName - must not be blank";
+
+        testBadRequestError(json, expectedErrorMessage);
+    }
+
+    @Test
+    @DisplayName("bookIdAndNameIsMandatory")
+    void createLibraryEvent_BookIdAndBookNameMandatory() throws Exception {
+
+        Book book = Book.builder()
+                        .bookId(null)
+                        .bookAuthor("John Wick")
+                        .bookName(null)
+                        .build();
+
+        LibraryEvent libraryEvent = LibraryEvent.builder()
+                                                .libraryEventId(null)
+                                                .book(book)
+                                                .build();
+
+        String json = objectMapper.writeValueAsString(libraryEvent);
+
+        doNothing().when(libraryEventProducer).sendLibraryEventAsync(isA(LibraryEvent.class));
+
+        String expectedErrorMessage = "book.bookId - must not be null, book.bookName - must not be blank";
+
+        testBadRequestError(json, expectedErrorMessage);
+    }
+
+    private void testBadRequestError(String json, String expectedErrorMessage) throws Exception {
+        mockMvc.perform(post("/v1/libraryevent")
+                .content(json)
+                .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isBadRequest())
+        .andExpect(content().string(expectedErrorMessage));
     }
 
 }
