33825c3cdb6c27b777975e02ad18621eeb0c19d2
Abhinav Nath <abhinavnath@ymail.com>
2022-03-27 23:43:01 +0530

    Initialise project

---
+++ code/consumer-error-handling/src/main/java/com/codecafe/kafka/ConsumerErrorHandlingApplication.java
@@ -0,0 +1,13 @@
+package com.codecafe.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerErrorHandlingApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ConsumerErrorHandlingApplication.class, args);
+  }
+
+}
