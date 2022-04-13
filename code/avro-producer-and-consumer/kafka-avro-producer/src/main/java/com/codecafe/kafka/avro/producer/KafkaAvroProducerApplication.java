95a633df830c8f91e509600c3b942d655dac2d1f
Abhinav Nath <abhinavnath@ymail.com>
2022-04-13 11:34:21 +0530

    Add .editorconfig

--- code/avro-producer-and-consumer/kafka-avro-producer/src/main/java/com/codecafe/kafka/avro/producer/KafkaAvroProducerApplication.java
+++ code/avro-producer-and-consumer/kafka-avro-producer/src/main/java/com/codecafe/kafka/avro/producer/KafkaAvroProducerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class KafkaAvroProducerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(KafkaAvroProducerApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(KafkaAvroProducerApplication.class, args);
+  }
 
 }
