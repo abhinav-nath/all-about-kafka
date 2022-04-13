1366cb493789c1a426e036cf94d61b6c04dea923
Abhinav Nath <abhinavnath@ymail.com>
2022-04-13 16:39:56 +0530

    Create new project for Avro Consumer

---
+++ code/avro-producer-and-consumer/kafka-avro-consumer/src/main/java/com/codecafe/kafka/avro/KafkaAvroConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.codecafe.kafka.avro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaAvroConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaAvroConsumerApplication.class, args);
+	}
+
+}
