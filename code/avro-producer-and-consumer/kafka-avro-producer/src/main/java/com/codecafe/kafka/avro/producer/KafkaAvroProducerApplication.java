668e62c8cea9866a7bbdfbb5fdef5d074fc8c354
Abhinav Nath <abhinavnath@ymail.com>
2022-04-13 11:29:36 +0530

    Create kafka-avro-producer project

---
+++ code/avro-producer-and-consumer/kafka-avro-producer/src/main/java/com/codecafe/kafka/avro/producer/KafkaAvroProducerApplication.java
@@ -0,0 +1,13 @@
+package com.codecafe.kafka.avro.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaAvroProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaAvroProducerApplication.class, args);
+	}
+
+}
---
+++ code/avro-producer-and-consumer/kafka-avro-producer/src/test/java/com/codecafe/kafka/avro/producer/KafkaAvroProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecafe.kafka.avro.producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaAvroProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
