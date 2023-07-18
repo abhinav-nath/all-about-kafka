3355d202954b4990a40670a1c8f22225feaab115
Abhinav Nath <abhinavnath@ymail.com>
2023-07-18 23:26:48 +0530

    Bump Kafka version

--- code/simple-producer-and-consumer/src/main/java/com/codecafe/kafka/utils/Constants.java
+++ code/simple-producer-and-consumer/src/main/java/com/codecafe/kafka/utils/Constants.java
@@ -2,9 +2,10 @@ package com.codecafe.kafka.utils;
 
 public class Constants {
 
+  //  public static final String BOOTSTRAP_SERVERS = ":29092, localhost:29092";
+  public static final String BOOTSTRAP_SERVERS = "localhost:9092";
+
   private Constants() {
   }
-
-  public static final String BOOTSTRAP_SERVERS = ":29092, localhost:29092";
 
 }
